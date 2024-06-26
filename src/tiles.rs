1889aea05fb9f3fdea443c7c87f1622854764b30
maybeElse <93097725+maybeElse@users.noreply.github.com>
2024-06-26 12:11:39 -0400

    feat: helper functions for tile structs/enums
    this is a surprise tool that will help us later

--- src/tiles.rs
+++ src/tiles.rs
@@ -98,32 +98,149 @@ impl FromChar for Suit {
     }
 }
 
+trait TileHelpers {
+    fn is_terminal(self: &Self) -> bool;
+    fn adjacent_up(self: &Self) -> Option<[Tile; 2]>;
+    fn adjacent_down(self: &Self) -> Option<[Tile; 2]>;
+    fn adjacent_around(self: &Self) -> Option<[Tile; 2]>;
+}
+
+impl TileHelpers for SimpleTile {
+
+    fn is_terminal(self: &Self) -> bool {
+        match self.number {
+            1 | 9 => true,
+            _ => false,
+        }
+    }
+
+    fn adjacent_up(self: &Self) -> Option<[Tile; 2]> {
+        match self.number {
+            8 | 9 => None,
+            _ => {
+                let adj: [Tile; 2] = [
+                    Tile::Simple(SimpleTile{suit: self.suit, number: self.number + 1, red: false}),
+                    Tile::Simple(SimpleTile{suit: self.suit, number: self.number + 2, red: false})
+                ];
+                Some(adj)
+            }
+        }
+    }
+
+    fn adjacent_down(self: &Self) -> Option<[Tile; 2]> {
+        match self.number {
+            1 | 2 => None,
+            _ => {
+                let adj: [Tile; 2] = [
+                    Tile::Simple(SimpleTile{suit: self.suit, number: self.number - 1, red: false}),
+                    Tile::Simple(SimpleTile{suit: self.suit, number: self.number - 2, red: false})
+                ];
+                Some(adj)
+            }
+        }
+    }
+
+    fn adjacent_around(self: &Self) -> Option<[Tile; 2]> {
+        match self.number {
+            1 | 9 => None,
+            _ => {
+                let adj: [Tile; 2] = [
+                    Tile::Simple(SimpleTile{suit: self.suit, number: self.number + 1, red: false}),
+                    Tile::Simple(SimpleTile{suit: self.suit, number: self.number - 1, red: false})
+                ];
+                Some(adj)
+            }
+        }
+    }
+}
+
+trait DoraOf {
+    fn dora_of (self: &Self) -> Tile;
+}
+
+impl DoraOf for Tile {
+    fn dora_of (self: &Self) -> Tile {
+        match self {
+            Tile::Simple(simp) => {
+                if simp.number == 9 {
+                    Tile::Simple(SimpleTile{suit: simp.suit, number: 1, red: false})
+                } else {
+                    Tile::Simple(SimpleTile{suit: simp.suit, number: simp.number + 1, red: false})
+                }
+            },
+            Tile::Honor(hon) => {
+                match hon { // there should be a better way to do this
+                    HonorTile::Dragon(DragonTile::White) => Tile::Honor(HonorTile::Dragon(DragonTile::Green)),
+                    HonorTile::Dragon(DragonTile::Green) => Tile::Honor(HonorTile::Dragon(DragonTile::Red)),
+                    HonorTile::Dragon(DragonTile::Red) => Tile::Honor(HonorTile::Dragon(DragonTile::White)),
+                    HonorTile::Wind(WindTile::East) => Tile::Honor(HonorTile::Wind(WindTile::South)),
+                    HonorTile::Wind(WindTile::South) => Tile::Honor(HonorTile::Wind(WindTile::West)),
+                    HonorTile::Wind(WindTile::West) => Tile::Honor(HonorTile::Wind(WindTile::North)),
+                    HonorTile::Wind(WindTile::North) => Tile::Honor(HonorTile::Wind(WindTile::East)),
+                }
+            }
+        }
+    }
+}
+
 #[derive(Debug, PartialEq)]
+pub enum Meld {
+    Triplet(Triplet),
+    Sequence(Sequence),
+    Kan(Kan),
+    Pair(Pair)
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Triplet {
+    pub open: bool,
+    pub tile: Tile
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Sequence {
+    pub open: bool,
+    pub tiles: [Tile; 3]
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Kan {
+    pub open: bool,
+    pub tile: Tile
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Pair {
+    pub open: bool,
+    pub tile: Tile
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
 pub enum Tile {
     Simple(SimpleTile),
     Honor(HonorTile),
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 pub struct SimpleTile {
     pub suit: Suit,
     pub number: i8,
     pub red: bool,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 pub enum HonorTile {
     Dragon(DragonTile),
     Wind(WindTile)
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 pub enum Suit {Man, Sou, Pin,}
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 pub enum DragonTile {White, Green, Red,}
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 pub enum WindTile {East, South, West, North,}
 
 mod tests {
